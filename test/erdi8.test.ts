1668ed5b3115d415db14e6f1b5d231cf6e5f366a
Andreas Thalhammer <andreas@thalhammer.bayern>
2023-07-21 22:14:12 +0200

    added further test for safe=True

--- test/erdi8.test.ts
+++ test/erdi8.test.ts
@@ -4,12 +4,10 @@ import { Erdi8 } from "../src/erdi8";
 import * as fs from "fs";
 import * as path from "path";
 
-describe('testing erdi8 file', () => {
+describe('testing erdi8', () => {
     test('check list of expected values', () => {
         var csv = fs.readFileSync(path.join(__dirname, "test.csv"), "utf8");
-        //console.log(csv);
         var rows = csv.split("\n");
-        //console.log(rows);
         var e8 = new Erdi8();
 
         for (var i = 0; i < rows.length; i++) {
@@ -25,4 +23,24 @@ describe('testing erdi8 file', () => {
             expect(str).toBe(enc);
         }
     });
+
+    test('check list of expected values for safe = True', () => {
+        var csv = fs.readFileSync(path.join(__dirname, "test_safe.csv"), "utf8");
+        var rows = csv.split("\n");
+        var e8 = new Erdi8(true);
+
+        for (var i = 0; i < rows.length; i++) {
+            var row = rows[i];
+            var cols = row.split(",");
+            var num = parseInt(cols[0]);
+            var str = (cols[1]);
+
+
+            var enc = e8.encodeInt(num);
+            var dec = e8.decodeInt(str);
+            expect(num).toBe(dec);
+            expect(str).toBe(enc);
+        }
+    });
+
   });
